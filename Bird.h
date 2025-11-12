35eecb9558e3639d780335281509b3a6c0de4e05
Hagarosama16001781 <hagar.baher@student.giu-uni.de>
2025-11-12 15:18:14 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -1,14 +1,17 @@
 #ifndef BIRD_H
 #define BIRD_H
-
-#include "Animal.h"
-
-//define class bird here 
-
-
-
-
-
-
-
+#include"animal.h"
+#include<iostream>
+using namespace std;
+class bird:public animal{
+    private:
+    float wingspan;
+    public:
+    bird();
+    bird(string name,int age,bool ishungry,float wingspan);
+    ~bird();
+    float setwingspan(float w);
+    void getwingspan();
+    
+};
 #endif
