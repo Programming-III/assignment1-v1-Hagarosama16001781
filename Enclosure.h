a74950f3d6a46ad42f40c96ac931dab92edeed4f
Hagarosama16001781 <hagar.baher@student.giu-uni.de>
2025-11-12 15:18:43 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,19 @@
 #ifndef ENCLOSURE_H
 #define ENCLOSURE_H
-
-#include "Animal.h"
-
-//define enclosure class here 
-
-
-
-
-
-
-
-
+#include"animal.h"
+#include<iostream>
+using namespace std;
+class enclosure{
+    private:
+    animal*a;
+    int capacity;
+    int currentcount;
+    public:
+    enclosure();
+    enclosure(int capacity, int currentcount);
+    ~enclosure();
+    void addaniaml(animal*a);
+    void displayanimal();
+    
+};
 #endif
