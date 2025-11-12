7d772a92b82ed14b838d1eb8cf150183c6e7bec4
Hagarosama16001781 <hagar.baher@student.giu-uni.de>
2025-11-12 15:19:16 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,17 @@
 #ifndef MAMMAL_H
 #define MAMMAL_H
-
-#include "Animal.h"
-
-//define mammal class here
-
-
-
-
-
-
+#include "animal.h"
+#include<iostream>
+using namespace std ;
+class mammal:public animal{
+    private:
+    string furcolor;
+    public:
+    mammal();
+    mammal(string name,int age,bool ishungry,string furcolor);
+    ~mammal();
+    string setfurcolor(string f);
+    void getfurcolor();
+    
+};
 #endif
